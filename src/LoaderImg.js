15b4e9bc6c486f9ace9d4c24c00eccf53ea83e6c
JanaStepanchak <jana.stepanchak@gmail.com>
2020-06-23 09:30:35 +0300

    f2 commit

--- src/LoaderImg.js
+++ src/LoaderImg.js
@@ -6,35 +6,32 @@ class LoaderImg extends Component {
         super (props);
         this.state = {
             src:"",
-            alt:"", 
             loaded: false
          }
     } 
 
 authenticate(){
-    return new Promise(resolve => setTimeout(resolve, 2000))
+    return new Promise(resolve => setTimeout(resolve, 100))
   }
 
   componentDidMount(){
     this.authenticate().then(() => {
            this.setState({
             src : 'http://img.lenagold.ru/tc/tcvet/gelkr/gelkr_tcvet045.png' ,              
-            alt : "альтернативный текст",
             loaded: true
         })
-        }, 0)
+        })
       }
 
     render = () => {
-        const {  loaded, src, alt } = this.state;
+        const {  loaded, src } = this.state;
         if( !loaded ){
             return( <h1> Loading... </h1> );
         } else {
             return(
                 <>
                     <img
-                    src ={src}                   
-                    alt={alt}
+                    src ={src}                 
                     width={200}
                     height={200}                   
                     > 
